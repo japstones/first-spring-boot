a201174e2c8564d9d8a1272419924d65ba1ede98
japstones <joelpovis@gmail.com>
2015-11-12 23:35:57 -0500

    Primer commit

---
+++ src/main/java/pe/jp/spring/FirstsPringBootApplication.java
@@ -0,0 +1,12 @@
+package pe.jp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsPringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstsPringBootApplication.class, args);
+    }
+}
